99158cbad6ec2b444a645865c9f181cb93bd4ed6
songsong xu <>
2019-07-02 15:03:59 +0800

    login/detail详情

--- app/containers/Detail.js
+++ app/containers/Detail.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native';
 import { connect } from 'react-redux'
-
-import { Button } from '../components'
-
-import { NavigationActions } from '../utils'
-
+import AutoFlatList from 'react-native-autoflatlist'
 @connect()
-class Detail extends Component {
+class lists extends Component {
   static navigationOptions = {
-    title: 'Detail',
+    title: 'lists',
   }
+  loadData(page) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let data = [];
+            for (let i = 0; i < 10; i++) {
+                data.push(`页数${page},数据源${i}`)
+            }
 
-  gotoDetail = () => {
-    this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
-  }
+            //模拟没有更多数据
+            //data.splice(5, 3)
 
-  goBack = () => {
-    this.props.dispatch(NavigationActions.back({ routeName: 'Account' }))
-  }
+            resolve(data)
+        }, 1000)
+        })
+    }
 
+    renderItem(item, index) {
+        return <Text style={{fontSize: 28, padding: 40, backgroundColor: 'white', marginTop: 1}}>{item}</Text>
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Button text="Goto Detail" onPress={this.gotoDetail} />
-        <Button text="Go Back" onPress={this.goBack} />
-      </View>
+      <AutoFlatList
+             style={{flex: 1,backgroundColor: '#eee'}}
+             netWork={(page) => this.loadData(page)}
+             loadMoreEnable={true}
+             renderItem={({item, index}) => this.renderItem(item, index)}
+         />
     )
   }
 }
@@ -38,4 +46,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default Detail
+export default lists
--- app/containers/Login.js
+++ app/containers/Login.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Text, ActivityIndicator ,TextInput} from 'react-native'
 import { connect } from 'react-redux'
 import { createAction, NavigationActions } from '../utils'
-
+import { Button,Touchable } from '../components'
 @connect(({ app }) => ({ ...app }))
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: '',
+        phone: '',
+        isCanLogin: false
+    };
+}
   static navigationOptions = {
     title: 'Login',
   }
 
   onLogin = () => {
-    this.props.dispatch(createAction('app/login')())
+    //this.props.dispatch(createAction('app/login')())
+    this.props.dispatch(NavigationActions.navigate({ routeName: 'lists' }))
   }
 
   onClose = () => {
@@ -24,16 +33,29 @@ class Login extends Component {
         {fetching ? (
           <ActivityIndicator />
         ) : (
-          <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({text})}
-        />
+          <View>
+          
+            <TextInput
+            placeholder="请填写昵称"
+            style={styles.btn}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.name}
+          />
+           <TextInput
+            placeholder="请填写密码"
+            style={styles.btn}
+            password={true}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.phone}
+          />
+            <Button text="Login" onPress={this.onLogin} />
+          </View>
         )}
-        {/* {!fetching && (
+        {!fetching && (
           <Touchable style={styles.close} text="Close" onPress={this.onClose}>
             <Text>Close</Text>
           </Touchable>
-        )} */}
+        )}
       </View>
     )
   }
@@ -50,6 +72,12 @@ const styles = StyleSheet.create({
     right: 20,
     top: 40,
   },
+  btn: {
+    marginBottom:10,
+    borderWidth:0.8,
+    width:200,
+    height:32
+  }
 })
 
 export default Login
